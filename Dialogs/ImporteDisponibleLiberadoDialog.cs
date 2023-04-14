08434da8bd512d6c8cb7ad64a736c5e072bc31bb
ANGEL MANUEL TAPIA AVITIA <angel.tapiaa@ext.arcacontal.com>
2023-04-14 14:21:43 -0600

    Cambios formato visualizacion

--- Dialogs/ImporteDisponibleLiberadoDialog.cs
+++ Dialogs/ImporteDisponibleLiberadoDialog.cs
@@ -5,6 +5,7 @@ using System;
 using Bot.Api.Data;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using AdaptiveCards;
 
 namespace Bot.Api.Dialogs
 {
@@ -87,7 +88,7 @@ namespace Bot.Api.Dialogs
             return await stepContext.PromptAsync(nameof(TextPrompt), promptOptions, cancellationToken);
         }
 
-        private Task<DialogTurnResult> ShowUsers(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> ShowUsers(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             //Creamos la instancia para la conexion 
             var db = new DatabaseService("sqlserverdac.database.windows.net", "databaseac", "usrteam1", "XW9ZEzoa");
@@ -99,7 +100,7 @@ namespace Bot.Api.Dialogs
 
             var tableName = society switch
             {
-                "DAC" => "[Dummy_DAC]",
+                "DAC" => "[DummyDAC]",
                 "AC SAB" => "[Dummy_AC_SAB]",
                 "SAB" => "[Dummy_AC_SAB]",
                 "AC_SAB" => "[Dummy_AC_SAB",
@@ -107,7 +108,7 @@ namespace Bot.Api.Dialogs
             };
 
             // Construct the query using the CeCo and NumCuenta parameters
-            var query = $@"SELECT Centro_Gestor, Pos_Pre, Importe_Disponible_Anual_Dipsonible_al_momento
+            var query = $@"SELECT Centro_Gestor, Pos_Pre, Importe_Disponible_Anual_Disponible_al_momento
                    FROM {tableName}
                    WHERE Centro_Gestor = '{ceco}'
                    AND Pos_Pre = '{numCuenta}'";
@@ -120,25 +121,60 @@ namespace Bot.Api.Dialogs
 
                 while (reader.Read())
                 {
-                    var CeCo = reader["Centro_Gestor"];
-                    var NumCuenta = reader["Pos_Pre"];
-                    var sociedad = reader["Society"];
-                    var saldoPresupuestal = reader["Importe_Disponible_Anual_Disponible_al_momento"];
+                    var CeCo = reader["Centro_Gestor"].ToString();
+                    var NumCuenta = reader["Pos_Pre"].ToString();
+                    var sociedad = society;
+                    var saldoPresupuestal = reader["Importe_Disponible_Anual_Disponible_al_momento"].ToString();
 
-                    stepContext.Context.SendActivityAsync($"Centro de Costos: {CeCo}, Numero de cuenta: {NumCuenta}, Sociedad: {sociedad}, Saldo Presupuestal: {saldoPresupuestal}");
+                    //await stepContext.Context.SendActivityAsync($"Centro de Costos: {CeCo}, Numero de cuenta: {NumCuenta}, Sociedad: {sociedad}, Saldo Presupuestal: {saldoPresupuestal}");
+
+                    var card = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0));
+                    var columnSet = new AdaptiveColumnSet();
+                    var column1 = new AdaptiveColumn() { Width = "20%" };
+                    var column2 = new AdaptiveColumn() { Width = "30%" };
+                    var column3 = new AdaptiveColumn() { Width = "25%" };
+                    var column4 = new AdaptiveColumn() { Width = "25%" };
+
+                    column1.Items.Add(new AdaptiveTextBlock() { Text = "Centro de Costos" });
+                    column1.Items.Add(new AdaptiveTextBlock() { Text = CeCo });
+
+                    column2.Items.Add(new AdaptiveTextBlock() { Text = "Numero de cuenta" });
+                    column2.Items.Add(new AdaptiveTextBlock() { Text = NumCuenta });
+
+                    column3.Items.Add(new AdaptiveTextBlock() { Text = "Sociedad" });
+                    column3.Items.Add(new AdaptiveTextBlock() { Text = sociedad });
+
+                    column4.Items.Add(new AdaptiveTextBlock() { Text = "Saldo Presupuestal" });
+                    column4.Items.Add(new AdaptiveTextBlock() { Text = saldoPresupuestal });
+
+                    columnSet.Columns.Add(column1);
+                    columnSet.Columns.Add(column2);
+                    columnSet.Columns.Add(column3);
+                    columnSet.Columns.Add(column4);
+
+                    card.Body.Add(columnSet);
+
+                    Attachment attachment = new Attachment()
+                    {
+                        ContentType = AdaptiveCard.ContentType,
+                        Content = card
+                    };
+
+                    var reply = MessageFactory.Attachment(attachment);
+                    await stepContext.Context.SendActivityAsync(reply);
                 }
 
                 reader.Close();
             }
             catch (Exception ex)
             {
-                stepContext.Context.SendActivityAsync("The bot encountered an error or bug.", cancellationToken: cancellationToken);
-                stepContext.Context.SendActivityAsync("To continue to run this bot, please fix the bot source code.", cancellationToken: cancellationToken);
-                stepContext.Context.SendActivityAsync(ex.Message, cancellationToken: cancellationToken);
+                await stepContext.Context.SendActivityAsync("The bot encountered an error or bug.", cancellationToken: cancellationToken);
+                await stepContext.Context.SendActivityAsync("To continue to run this bot, please fix the bot source code.", cancellationToken: cancellationToken);
+                await stepContext.Context.SendActivityAsync(ex.Message, cancellationToken: cancellationToken);
             }
 
 
-            return stepContext.NextAsync(cancellationToken: cancellationToken);
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
         }
 
         private Task<DialogTurnResult> End(WaterfallStepContext stepContext, CancellationToken cancellationToken)
