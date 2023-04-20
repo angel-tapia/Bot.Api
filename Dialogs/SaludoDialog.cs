d7f1e00139d1b5a43bb862d0bec0cab2c4e9db84
ANGEL MANUEL TAPIA AVITIA <angel.tapiaa@ext.arcacontal.com>
2023-04-20 14:47:04 -0600

    Reescribe todo de nuevo cuando acaba todo dialog

--- Dialogs/SaludoDialog.cs
+++ Dialogs/SaludoDialog.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
 using static Antlr4.Runtime.Atn.SemanticContext;
 
 namespace Bot.Api.Dialogs
@@ -20,7 +22,22 @@ namespace Bot.Api.Dialogs
 
         private async Task<DialogTurnResult> SaludoAsync(WaterfallStepContext dialog, CancellationToken cancellationToken)
         {
-            await dialog.Context.SendActivityAsync(MessageFactory.Text($"Hola! Estoy feliz por ayudarte, escribe tu pregunta o seleccionala del menu de arriba para que comencemos ;)"), cancellationToken);
+            await dialog.Context.SendActivityAsync(MessageFactory.Text($"Hola! Estoy feliz por ayudarte, escribe tu pregunta o seleccionala del menu de abajo para que comencemos ;)"), cancellationToken);
+            // Deploy a option's list to select or write the question
+            await dialog.Context.SendActivityAsync(MessageFactory.Text($"Elije una opción o escribe tu pregunta"), cancellationToken);
+            var card = new HeroCard
+            {
+                Text = "Aquí hay una variedad de opciones de las cuales puedes escoger:",
+                Buttons = new List<CardAction>
+                        {
+                            new CardAction(ActionTypes.ImBack, title: "1) Consultar tu saldo presupuestal", value: "Consulta saldo presupuestal"),
+                            new CardAction(ActionTypes.ImBack, title: "2) Solicitud de Traspaso", value: "Tengo una solicitud de traspaso"),
+                            new CardAction(ActionTypes.ImBack, title: "3) FAQs (Preguntas frecuentes)", value: "Muestrame las preguntas frecuentes"),
+                            new CardAction(ActionTypes.ImBack, title: "4) Solicitar Reporte Power BI", value: "Solicito un reporte Power BI")
+                        }
+            };
+            await dialog.Context.SendActivityAsync(MessageFactory.Attachment(card.ToAttachment()), cancellationToken);
+            await dialog.Context.SendActivityAsync(MessageFactory.Text($"O escribe tu propia pregunta =), solo está implementada la intención Consultar tu saldo presupuestal"), cancellationToken);
             return await dialog.EndDialogAsync(cancellationToken: cancellationToken);
         }
 
